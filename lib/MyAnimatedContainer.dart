9f689c98bdc97c18b432b35912d5820dafaabbbd
hyunwook <c004245@naver.com>
2019-10-11 20:34:09 +0900

    Use Random AnimatedContainer

---
+++ lib/MyAnimatedContainer.dart
@@ -0,0 +1,40 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class MyAnimatedContainer extends StatefulWidget {
+  @override
+  _AnimatedContainerState createState() => _AnimatedContainerState();
+}
+
+class _AnimatedContainerState extends State<MyAnimatedContainer> {
+  double _width = 50;
+  double _height = 50;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('AnimatedContainer'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+            width: _width,
+            height: _height,
+            color: Colors.green,
+            duration: Duration(seconds: 1)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            final random = Random();
+            _width = random.nextInt(300).toDouble();
+            _height = random.nextInt(300).toDouble();
+          });
+        },
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+
+  }
+}
