2784f98754b1938eeae780e2c347eeb96dac7f37
hyunwook <c004245@naver.com>
2019-10-12 16:42:16 +0900

    Use TextFleid, AlertDialog

--- lib/MyFormValidation.dart
+++ lib/MyFormValidation.dart
@@ -7,38 +7,88 @@ class MyFormValidation extends StatefulWidget {
 
 class _MyFormValidationState extends State<MyFormValidation> {
   final _formKey = GlobalKey<FormState>();
+  FocusNode nameFocusNode;
+  final nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nameFocusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    nameFocusNode.dispose();
+    nameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Form Validation'),
-      ),
-      body: Form(
-          key: _formKey,
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            validator: (value) {
-              if (value.isEmpty) {
-                return '공백은 허용되지 않습니다.';
-              }
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: RaisedButton(onPressed: () {
-              if (_formKey.currentState.validate()) {
-                //valid
-                Scaffold.of(_formKey.currentContext)
-                    .showSnackBar(SnackBar(content: Text('처리 중')));
-              }
-            },
-            child: Text('완료'),),
-          )
-        ],
-      )));
-
+        appBar: AppBar(
+          title: Text('Form Validation'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return '공백은 허용되지 않습니다.';
+                      }
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: RaisedButton(
+                      onPressed: () {
+                        if (_formKey.currentState.validate()) {
+                          //valid
+                          Scaffold.of(_formKey.currentContext)
+                              .showSnackBar(SnackBar(content: Text('처리 중')));
+                        }
+                      },
+                      child: Text('완료'),
+                    ),
+                  ),
+                  TextField(
+                    controller: nameController,
+                    onChanged: (text) {
+                      print(text);
+                    },
+                    focusNode: nameFocusNode,
+                    decoration: InputDecoration(
+                        hintText: '이름을 입력해 주세요',
+                        border: InputBorder.none,
+                        labelText: '이름'),
+                    autofocus: true,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      FocusScope.of(context).requestFocus(nameFocusNode);
+                    },
+                    child: Text('포커스'),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      print(nameController.text);
+                      showDialog(
+                          context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              content: Text(nameController.text),
+                            );
+                          });
+                    },
+                    child: Text('TextField 값 확인'),
+                  ),
+                ],
+              )),
+        ));
   }
 }
