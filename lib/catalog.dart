b099eaf6ec70d8293c6143322e903f018f0ea148
hyunwook <c004245@naver.com>
2019-10-13 23:26:36 +0900

    Create Model Item

---
+++ lib/catalog.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class Catalog extends StatefulWidget {
+  @override
+  _CatalogState createState() => _CatalogState();
+}
+
+class _CatalogState extends State<Catalog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/model/Item.dart
@@ -0,0 +1,14 @@
+class Item {
+  final String title;
+  final int price;
+
+  Item(this.title, this.price);
+}
+
+final itemList = [
+  Item("MacBook", 2000000),
+  Item("Magic keyboard", 99000),
+  Item("Magic Mouse", 150000),
+  Item("ipad", 3000000),
+  Item("iphone", 500000),
+];
